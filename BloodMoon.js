1239f6f2a6c2837fc668164352a5ebe3db1177c0
spennyL92 <spencer.linn@pcc.edu>
2019-02-06 18:41:25 -0800

    updated moon

--- BloodMoon.js
+++ BloodMoon.js
@@ -1,7 +1,6 @@
 var r = 0;
 var w = 255;
-var circleX = 0;
-var circleY = 400;
+var days = 1;
 
 function setup() {
   createCanvas(400, 400);
@@ -10,27 +9,23 @@ function setup() {
 function draw() {
   background(0);
 	
-	bloodMoon();
+	//enter in a number between 1 and 30 to simulate phase 
+	bloodMoon(15);
 	
 }
 
-function bloodMoon(){
+function bloodMoon(moonPhase){
 	
+	//move the mouse to the right to summon the bloodmoon \"/
 	r = map(mouseX, 0, 400, 0, 255);
 	w = map(mouseX, 0, 400, 255, 0);
 	
+	let phase = map(moonPhase, 1, 30, 120, 200);
+	
 	fill(r, w, w); 
-	ellipse(200, circleY, 80, 80);
+	ellipse(200, 200, 80, 80);
 	
 	fill(0);
-	ellipse(circleX, 200, 80, 80);
+	ellipse(phase, 200, 80, 80);
 	
-	circleX = circleX + .27
-	circleY = circleY - .5 
-
-	if (circleY < 200){
-		
-		circleY = 200;
-	}
 }
-	
